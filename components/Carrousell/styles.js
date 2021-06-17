455238fd43aac2e51926080042d9b81d3946b978
Gerardo Garavito <garavitogerardo@gmail.com>
2021-06-16 20:52:19 -0500

    added carrousell images

--- components/Carrousell/styles.js
+++ components/Carrousell/styles.js
@@ -17,10 +17,16 @@ export const Frame = styled.div`
   object-fit: cover;
   display: flex;
   align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `
 
 export const Image = styled.img`
-  /* height: 400px; */
+  max-height: 400px;
+  height: auto;
+  max-width: 180px;
+  width: auto;
+  object-fit: cover;
 `
 
 export const Legend = styled.p`
